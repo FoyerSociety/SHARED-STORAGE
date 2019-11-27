adc5625f0a07e902424c810c7968899a689a4e08
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-11-27 03:03:41 +0300

    Migaration html en django

--- Application/models.py
+++ Application/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Utilisateur(models.Model):
+    mail = models.EmailField(unique=True)
+    username = models.CharField(max_length=50)
+    status = models.CharField (max_length=15)
+    niveau = models.CharField(max_length=3)
